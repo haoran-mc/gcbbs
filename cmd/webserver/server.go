9f8ef39c53f65843069d08749d83e97f29a69061
haoran <haoran.mc@outlook.com>
2022-09-02 00:29:34 +0800

    load template and use session

--- cmd/webserver/server.go
+++ cmd/webserver/server.go
@@ -3,7 +3,10 @@ package webserver
 import (
 	"log"
 
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
+	"github.com/haoran-mc/gcbbs/pkg/config"
 	"github.com/haoran-mc/gcbbs/pkg/utils"
 )
 
@@ -12,6 +15,13 @@ func Run() {
 
 	// Define functions to handle complex operations in the template
 	engine.SetFuncMap(utils.GetTemplateFuncMap())
+
+	engine.Static("/assets", "../assets")
+	engine.LoadHTMLGlob("../views/**/**/*")
+
+	// Use cookies to store sessions
+	store := cookie.NewStore([]byte(config.Conf.Session.Secret))
+	engine.Use(sessions.Sessions(config.Conf.Session.Name, store))
 
 	if err := engine.Run(":8082"); err != nil {
 		log.Fatalf("serevr running error: %v", err)
