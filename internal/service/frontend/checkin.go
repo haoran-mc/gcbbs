62bdd875eb736bd92f6e6a4bc3cb4c1fdfdc706c
haoran <haoran.mc@outlook.com>
2022-09-05 00:08:10 +0800

    encapsulate context

---
+++ internal/service/frontend/checkin.go
@@ -0,0 +1,126 @@
+package frontend
+
+import (
+	"log"
+	"time"
+
+	"errors"
+
+	"github.com/gin-gonic/gin"
+	"github.com/haoran-mc/gcbbs/internal/consts"
+	"github.com/haoran-mc/gcbbs/internal/model"
+	"github.com/haoran-mc/gcbbs/internal/service"
+	"github.com/haoran-mc/gcbbs/pkg/db"
+	time2 "github.com/haoran-mc/gcbbs/pkg/utils/time"
+	"gorm.io/gorm"
+)
+
+type sCheckin struct {
+	ctx *service.BaseContext
+}
+
+func CheckinService(ctx *gin.Context) *sCheckin {
+	return &sCheckin{
+		ctx: service.Context(ctx),
+	}
+}
+
+// Store 签到保存
+func (s *sCheckin) Store() error {
+	uid := s.ctx.Auth().ID // 获取当前用户的id
+
+	var checkin model.Checkins
+	f := model.Checkin().M.Where("user_id", uid).Find(&checkin) // 根据id查找用户的签到表
+	if f.Error != nil {
+		return f.Error
+	}
+
+	// 如果今天已经签过到了，提醒请勿重复签到
+	if checkin.ID > 0 && checkin.LastTime.Format("2006-01-02") >= time.Now().Format("2006-01-02") {
+		return errors.New("请勿重复签到")
+	}
+
+	err := db.DB.Transaction(func(tx *gorm.DB) error {
+		if checkin.ID > 0 { // 已经存在该用户的签到表
+			preDate := time.Now().AddDate(0, 0, -1).Format("2006-01-02")
+			data := map[string]interface{}{
+				"cumulative_days": gorm.Expr("cumulative_days + 1"),
+				"last_time":       time.Now(),
+			}
+			// 判断是否连续签到
+			if checkin.LastTime.Format("2006-01-02") == preDate {
+				data["continuity_days"] = gorm.Expr("continuity_days + 1")
+			} else {
+				data["continuity_days"] = 1
+			}
+
+			u := tx.Model(&model.Checkins{}).Where("id", checkin.ID).Where("last_time", checkin.LastTime).Updates(data)
+			if u.Error != nil || u.RowsAffected <= 0 {
+				log.Panicln(u.Error)
+				return errors.New("签到失败")
+			}
+		} else { // 该用户还没有签到表，第一次签到
+			c := tx.Model(&model.Checkins{}).Create(&model.Checkins{
+				UserId:         uid,
+				CumulativeDays: 1,
+				ContinuityDays: 1,
+				LastTime:       time.Now(),
+			})
+			if c.Error != nil || c.RowsAffected <= 0 {
+				log.Panicln(c.Error)
+				return errors.New("签到失败")
+			}
+		}
+
+		// 记录积分奖励日志
+		c := tx.Model(&model.IntegralLogs{}).Create(&model.IntegralLogs{
+			UserId:  uid,
+			Rewards: consts.CHECKINReward, // 1
+			Mode:    consts.CheckinMode,   // 以签到的方式获取积分
+		})
+		if c.Error != nil || c.RowsAffected <= 0 {
+			log.Panicln(c.Error)
+			return errors.New("签到失败")
+		}
+
+		// 更新用户积分数
+		u := tx.Model(&model.Users{}).Where("id", uid).Updates(map[string]interface{}{
+			"integral": gorm.Expr("integral + ?", consts.CHECKINReward),
+		})
+		if u.Error != nil || u.RowsAffected <= 0 {
+			log.Panicln(u.Error)
+			return errors.New("签到失败")
+		}
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// IsCheckin 今日是否签到
+func (s *sCheckin) IsCheckin() (bool, error) {
+	if !s.ctx.Check() {
+		return false, nil
+	}
+
+	date := time2.ToDateString(time.Now())
+
+	startAt := date + " 00:00:00"
+	endedAt := date + " 23:59:59"
+
+	var checkin *model.Checkins
+	f := model.Checkin().M.
+		Where("last_time >= ?", startAt).
+		Where("last_time <= ?", endedAt).
+		Where("user_id", s.ctx.Auth().ID).
+		Find(&checkin)
+	if f.Error != nil {
+		return false, f.Error
+	}
+
+	return checkin.ID > 0, nil
+}
