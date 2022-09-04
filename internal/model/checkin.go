eeb6a46feb69ca51f9d31f8cd64499285140f672
haoran <haoran.mc@outlook.com>
2022-09-05 00:13:40 +0800

    wrong LastTime's type is time.Time

--- internal/model/checkin.go
+++ internal/model/checkin.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"time"
+
 	"github.com/haoran-mc/gcbbs/pkg/db"
 	"gorm.io/gorm"
 )
@@ -8,10 +10,10 @@ import (
 // Checkins 签到
 type Checkins struct {
 	NoDeleteModel
-	UserId         uint64 `gorm:"column:user_id" db:"user_id" json:"user_id" form:"user_id"`                                 // 用户 ID
-	CumulativeDays uint64 `gorm:"column:cumulative_days" db:"cumulative_days" json:"cumulative_days" form:"cumulative_days"` // 累积签到（天）
-	ContinuityDays uint64 `gorm:"column:continuity_days" db:"continuity_days" json:"continuity_days" form:"continuity_days"` // 连续签到（天）
-	LastTime       uint64 `gorm:"column:last_time" db:"last_time" json:"last_time" form:"last_time"`                         // 最后签到时间
+	UserId         uint64    `gorm:"column:user_id" db:"user_id" json:"user_id" form:"user_id"`                                 // 用户 ID
+	CumulativeDays uint64    `gorm:"column:cumulative_days" db:"cumulative_days" json:"cumulative_days" form:"cumulative_days"` // 累积签到（天）
+	ContinuityDays uint64    `gorm:"column:continuity_days" db:"continuity_days" json:"continuity_days" form:"continuity_days"` // 连续签到（天）
+	LastTime       time.Time `gorm:"column:last_time" db:"last_time" json:"last_time" form:"last_time"`                         // 最后签到时间
 }
 
 type checkinModel struct {
